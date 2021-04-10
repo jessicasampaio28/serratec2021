274193d62ab7e30c512ea1a75f61d0c516420b37
jessicasampaio28 <jessicaferreira28@hotmail.com>
2021-04-10 18:56:58 -0300

    exercicios fase 6

---
+++ exerciciosfase6/tabuada.js
@@ -0,0 +1,11 @@
+// Vou tomar a tabuada! O aluno de matemática escolhe o número, e o sistema lhe ensina a contar.
+
+const ler = require("prompt-sync")();
+
+var valor = ler("Qual tabuada deseja ver ?");
+
+for (let index = 1; index <= 10; index++) {
+    var resultado = valor * index;
+    console.log(valor, "x", index, "=", resultado);    
+}
+  
